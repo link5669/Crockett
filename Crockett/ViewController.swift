73dd9b6623e2f61a66b2538c6de4e75297b49503
Miles Acquaviva <Miles@miles-macbook-pro.local>
2016-12-31 09:41:51 -0500

    Initial Commit

---
+++ Crockett/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crockett
+//
+//  Created by Miles Acquaviva on 12/31/16.
+//  Copyright © 2016 Miles Acquaviva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
